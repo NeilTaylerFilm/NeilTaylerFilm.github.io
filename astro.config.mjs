d3e58fef498b6a8c348fe51eb04565efe14b5be1
Neil Tayler <neil@taylerfilm.com>
2026-09-13 00:48:31 +0100

    Fix site URL back to uppercase

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://neiltaylerfilm.github.io',
+	site: 'https://NeilTaylerFilm.github.io',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
